8886a6d133bdd302a68adc8ee8561c91da84f7e5
Valentin <valio990210@gmail.com>
2023-02-21 10:29:44 +0200

    import

--- src/app.js
+++ src/app.js
@@ -1,8 +1,26 @@
+ import { faces, suits, colors, Stock, Waste, Foundation, Pile } from '../cards.js'
+ 
  start()
  
  function start() {
-
     const state = {
-        
+        stock: new Stock(),
+        waste: new Waste(),
+        foundations: {
+            [suits.Clubs]: new Foundation([], suits.Clubs),
+            [suits.Diamonds]: new Foundation([], suits.Diamonds),
+            [suits.Spades]: new Foundation([], suits.Spades),
+            [suits.Hearts]: new Foundation([], suits.Hearts),
+        },
+        piles: [
+            new Pile(),
+            new Pile(),
+            new Pile(),
+            new Pile(),
+            new Pile(),
+            new Pile(),
+            new Pile(),
+        ] 
     }
+
  }
